e84f4c821c07c7507703a675e680e395ba8f7ca8
kamssa <kamssa0@gmail.com>
2021-06-01 01:04:55 +0000

    modif url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://server-e-com.herokuapp.com/'
+  apiUrl: 'https://server-e-com.herokuapp.com'
 };
 
 /*
